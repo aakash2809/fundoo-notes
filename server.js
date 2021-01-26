6d62f1dca1ca904c579021dc118c9e5d62646239
aakash2809 <aakashrajak289@gmail.com>
2021-01-26 07:59:32 +0530

    add: .env file and use it in server.js to connect server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+
+require(`dotenv`).config();
 const express = require('express')
-const app = express();
+const app     = express();
+const PORT    = process.env.PORT || 3000;
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000);
+//listen for request
+app.listen(PORT, () => {
+    console.log(`CONNECT_SERVER: Connected, server started listening on port : ${PORT}`);
+  });
