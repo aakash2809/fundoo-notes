6ce78a4a311f026a53a1b866b0fa476a3f89695d
aakash2809 <aakashrajak289@gmail.com>
2021-02-21 02:24:28 +0530

    fix : deprication warning to connect with mongodb

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.listen(PORT, () => {
   console.log(`CONNECT_SERVER: Connected, server started listening on port : ${PORT}`);
 });
 
-new dbconnection(process.env.MONGODB_URL, { useNewUrlParser: true }, { useUnifiedTopology: true }, { useFindAndModify: false }).connect();
+new dbconnection(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).connect();
 
 //Initialize the route
 userRoute.routeToUserController(app);
