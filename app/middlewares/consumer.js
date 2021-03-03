5d2f98acc5818eb9ad15335824882767c901ded7
aakash2809 <aakashrajak289@gmail.com>
2021-03-03 22:29:34 +0530

    refactor : mail content saving to queue then send to user done,impelementation with user method in progress

--- app/middlewares/consumer.js
+++ app/middlewares/consumer.js
@@ -1,5 +1,5 @@
 
-const amqp = require("amqplib/callback_api");
+/* const amqp = require("amqplib/callback_api");
 
 class Subscriber {
     consumeMessage = (callback) => {
@@ -16,7 +16,7 @@ class Subscriber {
                     let queueName = "EmailInQueues1";
                     channel.assertQueue(queueName);
                     channel.consume(queueName, (msg) => {
-                        console.log("mess");
+            
                         console.log(`Message consumes: ${msg.content.toString()}`);
                         channel.ack(msg);
                         return callback(null, msg.content.toString());
@@ -28,4 +28,97 @@ class Subscriber {
         }
     };
 }
-module.exports = new Subscriber();
+module.exports = new Subscriber(); */
+
+//const config = require('./config');
+const amqplib = require('amqplib/callback_api');
+const nodemailer = require('nodemailer');
+
+// Setup Nodemailer transport
+const transport = nodemailer.createTransport(/* {
+    host: "http://localhost/3000",
+    port: "3000",
+
+    // we intentionally do not set any authentication
+    // options here as we are going to use message specific
+    // credentials
+
+    // Security options to disallow using attachments from file or URL
+    disableFileAccess: true,
+    disableUrlAccess: true
+}, {
+    // Default options for the message. Used if specific values are not set
+    from: "aakashrajak2809@gmail.com"
+} */
+    {
+        service: "gmail",
+        //port: process.env.PORT,
+        secure: true, // use SSL
+        /*  auth: {
+             user: process.env.EMAIL_USER,
+             pass: process.env.EMAIL_PASS,
+         }, */
+    }
+
+);
+
+// Create connection to AMQP server
+amqplib.connect("amqp://localhost", (err, connection) => {
+    if (err) {
+        console.error(err.stack);
+        return process.exit(1);
+    }
+    // Create channel
+    connection.createChannel((err, channel) => {
+        if (err) {
+            console.error(err.stack);
+            return process.exit(1);
+        }
+
+        // Ensure queue for messages
+        channel.assertQueue("EmailInQueues1", {
+            // Ensure that the queue is not deleted when server restarts
+            durable: true
+        }, err => {
+            if (err) {
+                console.error(err.stack);
+                return process.exit(1);
+            }
+
+            // Only request 1 unacked message from queue
+            // This value indicates how many messages we want to process in parallel
+            channel.prefetch(1);
+
+            // Set up callback to handle messages received from the queue
+            channel.consume("EmailInQueues1", data => {
+                if (data === null) {
+                    return;
+                }
+
+                // Decode message contents
+                let message = JSON.parse(data.content.toString());
+
+                // attach message specific authentication options
+                // this is needed if you want to send different messages from
+                // different user accounts
+                message.auth = {
+                    user: "aakashrajak2809@gmail.com",
+                    pass: "P@55worddell35",
+                };
+
+                // Send the message using the previously set up Nodemailer transport
+                transport.sendMail(message, (err, info) => {
+                    if (err) {
+
+                        console.log("getting error in sending data", err);
+                        // put the failed message item back to queue
+                        return channel.nack(data);
+                    }
+                    console.log('Delivered message %s', info.messageId);
+                    // remove message item from the queue
+                    channel.ack(data);
+                });
+            });
+        });
+    });
+});
--- app/middlewares/publisher.js
+++ app/middlewares/publisher.js
@@ -1,4 +1,4 @@
-const amqp = require('amqplib/callback_api');
+/* const amqp = require('amqplib/callback_api');
 
 class Publish {
     getMessage = (userInfo, callback) => {
@@ -35,4 +35,78 @@ class Publish {
 
 module.exports = new Publish();
 
+
+ */
+require("dotenv").config();
+var ejs = require("ejs");
+const amqplib = require('amqplib/callback_api');
+var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkFobWFkIEFsaSIsInVzZXJJZCI6IjYwMjhjNjQ1ODhlMTkyMWNkNDQ2YTMxYSIsImlhdCI6MTYxNDc2MTM5MywiZXhwIjoxNjE0ODQ3NzkzfQ.AMIkrlOycrXzJwyIanVOAdRzFXlv9H8y4ob9oGLGN1s"
+
+// Create connection to AMQP server
+amqplib.connect("amqp://localhost", (err, connection) => {
+
+    if (err) {
+        console.error(err);
+        return process.exit(1);
+    }
+
+    // Create channel
+    connection.createChannel((err, channel) => {
+        if (err) {
+            console.error(err.stack);
+            return process.exit(1);
+        }
+
+        // Ensure queue for messages
+        channel.assertQueue("EmailInQueues1", {
+            // Ensure that the queue is not deleted when server restarts
+            durable: true
+        }, err => {
+            if (err) {
+                console.error(err.stack);
+                return process.exit(1);
+            }
+
+            // Create a function to send objects to the queue
+            // Javascript object is converted to JSON and then into a Buffer
+            let sender = (content, next) => {
+                let sent = channel.sendToQueue("EmailInQueues1", Buffer.from(JSON.stringify(content)), {
+                    // Store queued elements on disk
+                    persistent: true,
+                    contentType: 'application/json'
+                });
+                if (sent) {
+                    return next();
+                } else {
+                    channel.once('drain', () => next());
+                }
+            };
+
+            // push 100 messages to queue
+            let sent = 0;
+            let sendNext = async () => {
+                let dataToSend = await ejs.renderFile(
+                    "app/views/forgotPassword.ejs",
+                    {
+                        name: "Raju",
+                        resetLink: `${process.env.CLIENT_URL}/resetpassword/${token}`,
+                    })
+                if (sent >= 2) {
+                    console.log('All messages sent!');
+                    // Close connection to AMQP server
+                    // We need to call channel.close first, otherwise pending
+                    // messages are not written to the queue
+                    return channel.close(() => connection.close());
+                }
+                sent++;
+                sender({
+                    to: 'aakashrajak2809@gmail.com',
+                    subject: 'Test message #' + sent,
+                    html: dataToSend
+                }, sendNext);
+            };
+            sendNext();
+        });
+    });
+});
 
