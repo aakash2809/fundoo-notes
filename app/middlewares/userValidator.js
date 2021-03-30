7282284a5e1237490f8507a00e3d84eb42b0a4a1
aakash2809 <aakashrajak289@gmail.com>
2021-03-30 07:58:04 +0530

    refactor : add validation for last name

--- app/middlewares/userValidator.js
+++ app/middlewares/userValidator.js
@@ -11,6 +11,7 @@ const joi = require('joi');
 
 module.exports = joi.object({
     name: joi.string().required().regex(/^[A-Z]{1}[a-zA-Z ]{2,}$/),
+    lastName: joi.string(),
     email: joi.string().required(),
     password: joi.string().required().regex(/^(?=.*[0-9])(?=.*[A-Z])(?=.*[\\~\\?\\.\\+\\-\\~\\!\\@\\#\\$\\%\\^\\&\\*\\_])[a-zA-Z0-9\\~\\?\\.\\+\\-\\~\\!\\@\\#\\$\\%\\^\\&\\*\\_]{8,}$/),
     confirmPassword: joi.string().required()
