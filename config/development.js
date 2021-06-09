29325db381b6d06e7442c50910f7507eaddddddd
aakash2809 <aakashrajak289@gmail.com>
2021-06-09 09:12:26 +0530

    refactor : add necessary secret keys from dotenv

--- config/development.js
+++ config/development.js
@@ -7,6 +7,11 @@ require('dotenv').config();
 module.exports = () => ({
   port: process.env.DEV_PORT,
   MONGODB_URL: process.env.MONGODB_URL,
+  AMQP_CONNECTION: process.env.AMQP_CONNECTION,
+  CLIENT_URL: process.env.CLIENT_URL,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+
   logger: winston.createLogger({
     format: winston.format.json(),
     transports: [
