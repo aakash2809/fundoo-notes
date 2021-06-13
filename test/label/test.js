9ce31182d8706b6544aca85dd4c044df7f29af49
aakash2809 <aakashrajak289@gmail.com>
2021-06-14 00:11:48 +0530

    refactor : rename testing sample json file

--- test/label/test.js
+++ test/label/test.js
@@ -10,7 +10,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../../server');
-const testSamples = require('./sampleTest.json');
+const testSamples = require('./testSamples.json');
 const responseCode = require('../../util/staticFile.json');
 
 chai.should();
