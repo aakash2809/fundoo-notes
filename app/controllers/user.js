b88781dfec2ceff6d4096d2de5afb3486c545e99
aakash2809 <aakashrajak289@gmail.com>
2021-06-16 08:45:17 +0530

    refactor : remove user detail from response after login

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -98,7 +98,6 @@ class UserControllers {
             statusCode: loginResult.statusCode,
             message: loginResult.message,
             token: loginResult.data,
-            user: loginResult.user,
           });
       },
     );
